d54502b4bc3c1b708adb37168aef8995b4e5bd72
Malik Webster <mwebster30@gmail.com>
2024-09-19 18:18:17 -0500

    creating function to test endpoint

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -54,13 +54,27 @@
 	let name: string;
 	let email: string;
     let error: string;
+
+    async function getData() {
+        const url = "https://frontend-take-home-service.fetch.com/login";
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            console.log(json);
+        } catch (e: any) {
+            console.error(e.message);
+        }
+    }
 </script>
 
 <div class="container mt-4 flex flex-wrap justify-center mx-auto pt-16">
 	<div
 		class="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 w-full"
 	>
-		<form class="space-y-6" on:submit|preventDefault={login}>
+		<form class="space-y-6">  <!-- on:submit|preventDefault={login} -->
 			<div class="heading">
 				<a class="back" href="/"><i class="bi bi-arrow-left dark:text-white" /></a>
 				<h2 class="text-xl font-medium text-gray-900 dark:text-white">Login</h2>
@@ -93,14 +107,18 @@
 					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
 				/>
 			</div>
-			{#if error}
+			<!-- {#if error}
 				<div class="mt-6">
 					<Alert alertId="alert-red" color="red" closeBtn>
 						{error}
 					</Alert>
 				</div>
-			{/if}
-			<button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+			{/if} -->
+			<button
+                type="submit"
+                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                on:click={getData}
+            >
                 Log in
             </button>
 		</form>
